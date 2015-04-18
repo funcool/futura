acc27979fc1417f030730c5915cd0445a24c2319
Andrey Antukh <niwi@niwi.be>
2015-04-18 19:36:14 +0200

    Add missing dependencies on project.clj

--- project.clj
+++ project.clj
@@ -3,14 +3,16 @@
   :url "https://github.com/funcool/futura"
   :license {:name "BSD (2 Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
-  :dependencies [[cats "0.4.0"]]
+  :dependencies [[cats "0.4.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.reactivestreams/reactive-streams "1.0.0.RC5"]]
   :deploy-repositories {"releases" :clojars
                         "snapshots" :clojars}
-
   :source-paths ["src"]
   :test-paths ["test"]
   :jar-exclusions [#"\.swp|\.swo|user.clj"]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]
+  :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0-beta1"]]
                    :codeina {:sources ["src"]
                              :language :clojure
                              :output-dir "doc/api"}
