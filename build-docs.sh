52509e18df8fed024dc341d743a2de4bf1af434f
Andrey Antukh <niwi@niwi.be>
2015-04-21 21:34:23 +0200

    Improved build-docs.sh script.

--- build-docs.sh
+++ build-docs.sh
@@ -6,7 +6,8 @@ lein doc
 
 rm -rf /tmp/futura-doc/
 mkdir -p /tmp/futura-doc/
-mv doc/*.html /tmp/futura-doc/
+mv doc/index.html /tmp/futura-doc/
+mv doc/api /tmp/futura-doc/api
 
 git checkout gh-pages;
 
