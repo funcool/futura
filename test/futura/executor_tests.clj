2babcce0ca39a2a84fc30032620ff00290975fe2
Andrey Antukh <niwi@niwi.nz>
2015-06-23 16:18:55 +0300

    Add tests for executor api.

---
+++ test/futura/executor_tests.clj
@@ -0,0 +1,27 @@
+(ns futura.executor-tests
+  (:require [clojure.core.async :as a]
+            [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [clojure.pprint :refer [pprint]]
+            [futura.executor :as exec]))
+
+(deftest iexecutor-tests
+  (let [p (promise)]
+    (exec/execute #(deliver p true))
+    (is (deref p 1000 false)))
+
+  (let [p (promise)]
+    (exec/execute exec/*default* #(deliver p true))
+    (is (deref p 1000 false))))
+
+(deftest iexecutor-service-tests
+  (let [p (exec/submit (fn []
+                         (a/<!! (a/timeout 500))
+                         true))]
+    (is (deref p 1000 false)))
+  (let [submit (partial exec/submit exec/*default*)
+        p (submit (fn []
+                    (a/<!! (a/timeout 500))
+                    true))]
+    (is (deref p 1000 false))))
+
